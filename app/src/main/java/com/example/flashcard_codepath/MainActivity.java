4b2730af8b78f2f2fc2e9d869250fc72c7ee8219
MahtaE <mahta.ellis@student.csulb.edu>
2020-03-28 00:03:12 -0700

    Update codes

--- app/src/main/java/com/example/flashcard_codepath/MainActivity.java
+++ app/src/main/java/com/example/flashcard_codepath/MainActivity.java
@@ -8,8 +8,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.animation.Animation;
@@ -28,29 +26,25 @@ public class MainActivity extends AppCompatActivity {
     private Flashcard cardToEdit;
     private FlashcardDatabase flashcardDatabase;
 
+    private final CountDownTimer countDownTimer = new CountDownTimer(16000, 1000) {
+        public void onTick(long millisUntilFinished) {
+            ((TextView) findViewById(R.id.timer)).setText("" + millisUntilFinished / 1000);
+        }
+
+        public void onFinish() {
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final CountDownTimer countDownTimer; // why final?
-        countDownTimer = new CountDownTimer(16000, 1000) {
-            public void onTick(long millisUntilFinished) {
-                ((TextView) findViewById(R.id.timer)).setText("" + millisUntilFinished / 1000);
-            }
-            private void startTimer() {
-                countDownTimer.cancel();
-                countDownTimer.start();
-            }
-
-            public void onFinish() {
-            }
-        };
 
         flashcardDatabase = new FlashcardDatabase(this);
         allFlashcards = flashcardDatabase.getAllCards();
 
         if (allFlashcards != null && allFlashcards.size() > 0) {
-            ((TextView) findViewById(R.id.questionText)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
             ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
             ((Button) findViewById(R.id.ans1)).setText(allFlashcards.get(0).getWrongAnswer1());
             ((Button) findViewById(R.id.ans2)).setText(allFlashcards.get(0).getWrongAnswer2());
@@ -77,7 +71,25 @@ public class MainActivity extends AppCompatActivity {
                 Animator anim = ViewAnimationUtils.createCircularReveal(answerSideView, cx, cy, 0f, finalRadius);
 
 //              hide the question and show the answer to prepare for playing the animation!
-                View questionSideView = findViewById(R.id.flashcard_question);
+                final View questionSideView = findViewById(R.id.flashcard_question);
+                questionSideView.animate()
+                        .rotationY(90)
+                        .setDuration(200)
+                        .withEndAction(
+                                new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        questionSideView.setVisibility(View.INVISIBLE);
+                                        findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                                        // second quarter turn
+                                        findViewById(R.id.flashcard_answer).setRotationY(-90);
+                                        findViewById(R.id.flashcard_answer).animate()
+                                                .rotationY(0)
+                                                .setDuration(200)
+                                                .start();
+                                    }
+                                }
+                        ).start();
                 questionSideView.setVisibility(View.INVISIBLE);
                 answerSideView.setVisibility(View.VISIBLE);
 
@@ -101,14 +113,16 @@ public class MainActivity extends AppCompatActivity {
                         findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
                         findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
                     }
+
                     @Override
                     public void onAnimationEnd(Animation animation) {
                         // this method is called when the animation is finished playing
-                        ((TextView)findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
-                        ((TextView)findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                        ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                        ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
                         findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
                         findViewById(R.id.flashcard_question).startAnimation(rightInAnim);
                     }
+
                     @Override
                     public void onAnimationRepeat(Animation animation) {
                         // we don't need to worry about this method
@@ -116,60 +130,68 @@ public class MainActivity extends AppCompatActivity {
                 });
                 findViewById(R.id.flashcard_question).startAnimation(leftOutAnim);
 
-        findViewById(R.id.deleteBtn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(allFlashcards.size() <= 1) {
-                    Toast.makeText(MainActivity.this, "Cannot delete last remaining flashcard!", Toast.LENGTH_LONG).show();
+                findViewById(R.id.deleteBtn).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (allFlashcards.size() <= 1) {
+                            Toast.makeText(MainActivity.this, "Cannot delete last remaining flashcard!", Toast.LENGTH_LONG).show();
 
-                } else {
-                    flashcardDatabase.deleteCard(((TextView) findViewById(R.id.questionText)).getText().toString());
+                        } else {
+                            flashcardDatabase.deleteCard(((TextView) findViewById(R.id.flashcard_question)).getText().toString());
 
-                    allFlashcards = flashcardDatabase.getAllCards();
-                    showNextCard();
-                }
+                            allFlashcards = flashcardDatabase.getAllCards();
+                            showNextCard();
+
+                        }
+
+                    }
+                });
+
+                findViewById(R.id.ans1).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        resetState();
+                        ((Button) findViewById(R.id.ans1)).setTextColor(getResources().getColor(R.color.wrongAns));
+
+                    }
+                });
+                findViewById(R.id.ans2).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        resetState();
+                        ((Button) findViewById(R.id.ans2)).setTextColor(getResources().getColor(R.color.wrongAns));
+                    }
+                });
+                findViewById(R.id.ans3).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        resetState();
+                        ((Button) findViewById(R.id.ans3)).setTextColor(getResources().getColor(R.color.correctAns));
+                    }
+                });
+                findViewById(R.id.addImageButton).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                        startActivityForResult(intent, ADD_CARD_REQUEST_CODE);
+                        overridePendingTransition(R.anim.right_in, R.anim.left_out);
+                    }
+                });
+
+                findViewById(R.id.nextCardButton).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showNextCard();
+                    }
+                });
 
             }
         });
+    }
 
-        findViewById(R.id.ans1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetState();
-                ((Button) findViewById(R.id.ans1)).setTextColor(getResources().getColor(R.color.wrongAns));
-
-            }
-        });
-        findViewById(R.id.ans2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetState();
-                ((Button) findViewById(R.id.ans2)).setTextColor(getResources().getColor(R.color.wrongAns));
-            }
-        });
-        findViewById(R.id.ans3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetState();
-                ((Button) findViewById(R.id.ans3)).setTextColor(getResources().getColor(R.color.correctAns));
-            }
-        });
-        findViewById(R.id.addImageButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                startActivityForResult(intent, ADD_CARD_REQUEST_CODE);
-                overridePendingTransition(R.anim.right_in, R.anim.left_out);
-            }
-        });
-
-        findViewById(R.id.next_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showNextCard();
-            }
-        });
-
+    private void startTimer() {
+        countDownTimer.cancel();
+        countDownTimer.start();
     }
 
     public void showNextCard() {
@@ -182,7 +204,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // set the question and answer TextViews with data from the database
-        ((TextView) findViewById(R.id.questionText)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+        ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
         ((Button) findViewById(R.id.ans1)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer1());
         ((Button) findViewById(R.id.ans2)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer2());
         ((Button) findViewById(R.id.ans3)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
@@ -193,7 +215,7 @@ public class MainActivity extends AppCompatActivity {
 
     // returns a random number between minNumber and maxNumber, inclusive.
     // for example, if i called getRandomNumber(1, 3), there's an equal chance of it returning either 1, 2, or 3.
-    public int getRandomNumber ( int minNumber, int maxNumber){
+    public int getRandomNumber(int minNumber, int maxNumber) {
         Random rand = new Random();
         return rand.nextInt((maxNumber - minNumber) + 1) + minNumber;
     }
@@ -207,7 +229,7 @@ public class MainActivity extends AppCompatActivity {
             String newAnswer = data.getStringExtra("answer");
             String newOptionOne = data.getStringExtra("optionOne");
             String newOptionTwo = data.getStringExtra("optionTwo");
-            ((TextView) findViewById(R.id.questionText)).setText(newQuestion);
+            ((TextView) findViewById(R.id.flashcard_question)).setText(newQuestion);
             ((Button) findViewById(R.id.ans1)).setText(newOptionOne);
             ((Button) findViewById(R.id.ans2)).setText(newOptionTwo);
             ((Button) findViewById(R.id.ans3)).setText(newAnswer);
@@ -219,10 +241,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     private void resetState() {
-        ((Button)findViewById(R.id.ans1)).setTextColor(Color.BLACK);
-        ((Button)findViewById(R.id.ans2)).setTextColor(Color.BLACK);
-        ((Button)findViewById(R.id.ans3)).setTextColor(Color.BLACK);
+        ((Button) findViewById(R.id.ans1)).setTextColor(Color.BLACK);
+        ((Button) findViewById(R.id.ans2)).setTextColor(Color.BLACK);
+        ((Button) findViewById(R.id.ans3)).setTextColor(Color.BLACK);
     }
 }
